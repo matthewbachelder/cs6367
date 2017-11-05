5f46f3241f72f005948692b5b28a810e39e28caf
rfisher <rifisher@gorbash.us>
2017-11-05 15:01:00 -0600

    New mutators

---
+++ pitest/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/InvertComparatorsMutator.java
@@ -0,0 +1,75 @@
+/**
+ * Author(s): Matthew Bachelder, Vaishnavi Bhosale, Richard Fisher
+ * 
+ * 
+ */
+package org.pitest.mutationtest.engine.gregor.mutators;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+//import org.pitest.mutationtest.engine.MutationIdentifier;
+import org.pitest.mutationtest.engine.gregor.AbstractInsnMutator;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+import org.pitest.mutationtest.engine.gregor.AbstractJumpMutator.Substitution;
+
+public enum InvertComparatorsMutator implements MethodMutatorFactory {
+
+    INVERT_COMPARATORS_MUTATOR;
+
+  @Override
+  public MethodVisitor create(final MutationContext context,
+      final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+    return new InvertComparatorsMethodVisitor(this, context, methodVisitor);
+  }
+
+  @Override
+  public String getGloballyUniqueId() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public String getName() {
+    return name();
+  }
+
+}
+
+class InvertComparatorsMethodVisitor extends AbstractInsnMutator {
+
+  private static final String                     DESCRIPTION = "inverted comparator operator";
+  private static final Map<Integer, Substitution> MUTATIONS   = new HashMap<Integer, Substitution>();
+
+  static {
+	  //TODO: What should I change '=' to?
+    MUTATIONS.put(Opcodes.IFEQ, new Substitution(Opcodes.IFNE, DESCRIPTION));
+	MUTATIONS.put(Opcodes.IFNE, new Substitution(Opcodes.IFEQ, DESCRIPTION));
+	MUTATIONS.put(Opcodes.IF_ICMPNE, new Substitution(Opcodes.IF_ICMPEQ, DESCRIPTION));
+	MUTATIONS.put(Opcodes.IF_ICMPEQ, new Substitution(Opcodes.IF_ICMPNE, DESCRIPTION));
+	MUTATIONS.put(Opcodes.IF_ACMPEQ, new Substitution(Opcodes.IF_ACMPNE, DESCRIPTION));
+	MUTATIONS.put(Opcodes.IF_ACMPNE, new Substitution(Opcodes.IF_ACMPEQ, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IFLE, new Substitution(Opcodes.IFGT, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IFGE, new Substitution(Opcodes.IFLT, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IFGT, new Substitution(Opcodes.IFLE, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IFLT, new Substitution(Opcodes.IFGE, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IF_ICMPLE, new Substitution(Opcodes.IF_ICMPGT, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IF_ICMPGE, new Substitution(Opcodes.IF_ICMPLT, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IF_ICMPGT, new Substitution(Opcodes.IF_ICMPLE, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IF_ICMPLT, new Substitution(Opcodes.IF_ICMPGE, DESCRIPTION));
+  }
+
+  InvertComparatorsMethodVisitor(final MethodMutatorFactory factory,
+      final MutationContext context, final MethodVisitor delegateMethodVisitor) {
+    super(factory, context, delegateMethodVisitor);
+  }
+
+  @Override
+  protected Map<Integer, Substitution> getMutations() {
+    return MUTATIONS;
+  }
+
+}
---
+++ pitest/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/RemoveArithmeticOpMutator.java
@@ -0,0 +1,72 @@
+/**
+ * Author(s): Matthew Bachelder, Vaishnavi Bhosale, Richard Fisher
+ * 
+ * 
+ */
+package org.pitest.mutationtest.engine.gregor.mutators;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.pitest.mutationtest.engine.gregor.AbstractInsnMutator;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
+
+public enum RemoveArithmeticOpMutator implements MethodMutatorFactory {
+	INVERT_COMPARATORS_MUTATOR;
+	  @Override
+	  public MethodVisitor create(final MutationContext context,
+	      final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
+	    return new RemoveArithmeticOpMethodVisitor(this, context, methodVisitor);
+	  }
+
+	  @Override
+	  public String getGloballyUniqueId() {
+	    return this.getClass().getName();
+	  }
+
+	  @Override
+	  public String getName() {
+	    return name();
+	  }
+}
+
+class RemoveArithmeticOpMethodVisitor extends AbstractInsnMutator {
+
+  private static final String                     DESCRIPTION = "inverted comparator operator";
+  private static final Map<Integer, Substitution> MUTATIONS   = new HashMap<Integer, Substitution>();
+
+  static {
+	  //TODO: Change IADD, FADD, DADD, LADD,ISUB, FSUB, DSUB, LSUB, IMUL, FMUL, DMUL, LMUL, IDIV, FDIV, DDIV, LDIV.  Any others???  
+    MUTATIONS.put(Opcodes.IADD, new Substitution(Opcodes.ISUB, DESCRIPTION));
+    MUTATIONS.put(Opcodes.FADD, new Substitution(Opcodes.FSUB, DESCRIPTION));
+    MUTATIONS.put(Opcodes.DADD, new Substitution(Opcodes.DSUB, DESCRIPTION));
+    MUTATIONS.put(Opcodes.LADD, new Substitution(Opcodes.LSUB, DESCRIPTION));
+    MUTATIONS.put(Opcodes.ISUB, new Substitution(Opcodes.IADD, DESCRIPTION));
+    MUTATIONS.put(Opcodes.FSUB, new Substitution(Opcodes.FADD, DESCRIPTION));
+    MUTATIONS.put(Opcodes.DSUB, new Substitution(Opcodes.DADD, DESCRIPTION));
+    MUTATIONS.put(Opcodes.LSUB, new Substitution(Opcodes.LADD, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IMUL, new Substitution(Opcodes.IDIV, DESCRIPTION));
+    MUTATIONS.put(Opcodes.FMUL, new Substitution(Opcodes.FDIV, DESCRIPTION));
+    MUTATIONS.put(Opcodes.DMUL, new Substitution(Opcodes.DDIV, DESCRIPTION));
+    MUTATIONS.put(Opcodes.LMUL, new Substitution(Opcodes.LDIV, DESCRIPTION));
+    MUTATIONS.put(Opcodes.IDIV, new Substitution(Opcodes.IMUL, DESCRIPTION));
+    MUTATIONS.put(Opcodes.FDIV, new Substitution(Opcodes.FMUL, DESCRIPTION));
+    MUTATIONS.put(Opcodes.DDIV, new Substitution(Opcodes.DMUL, DESCRIPTION));
+    MUTATIONS.put(Opcodes.LDIV, new Substitution(Opcodes.LMUL, DESCRIPTION));
+  }
+
+  RemoveArithmeticOpMethodVisitor(final MethodMutatorFactory factory,
+      final MutationContext context, final MethodVisitor delegateMethodVisitor) {
+    super(factory, context, delegateMethodVisitor);
+  }
+
+  @Override
+  protected Map<Integer, Substitution> getMutations() {
+    return MUTATIONS;
+  }
+
+}
