a1c8ffee9105a66921710c0cdde48ef1f3fa4304
Matthew Bachelder <mtb140130@utdallas.edu>
2017-11-19 09:21:16 -0600

    completed UOI mutator

--- pitest/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/cs6367/UOIMutator.java
+++ pitest/pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/cs6367/UOIMutator.java
@@ -9,21 +9,24 @@
 //TODO: Implement Me!!!  Make sure the MATH mutator is the best starting point.
 package org.pitest.mutationtest.engine.gregor.mutators.cs6367;
 
+
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import org.pitest.mutationtest.engine.gregor.*;
+import org.pitest.mutationtest.engine.MutationIdentifier;
+import org.pitest.mutationtest.engine.gregor.MethodInfo;
+import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
+import org.pitest.mutationtest.engine.gregor.MutationContext;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
 
 public enum UOIMutator implements MethodMutatorFactory {
 
-  UOI_MUTATOR;
+  UOI__MUTATOR;
 
   @Override
   public MethodVisitor create(final MutationContext context,
                               final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
-    return new UOIMethodVisitor(this, methodInfo, context, methodVisitor);
+    return new UOIMutatorMethodVisitor(this, context, methodVisitor);
   }
 
   @Override
@@ -35,97 +38,95 @@ public enum UOIMutator implements MethodMutatorFactory {
   public String getName() {
     return name();
   }
-
 }
 
-class UOIMethodVisitor extends AbstractInsnMutator {
+// This operator works exclusively on function local variables, not object variables
+class UOIMutatorMethodVisitor extends MethodVisitor {
 
-  UOIMethodVisitor(final MethodMutatorFactory factory,
-                   final MethodInfo methodInfo, final MutationContext context,
-                   final MethodVisitor writer) {
-    super(factory, methodInfo, context, writer);
+  private final MethodMutatorFactory factory;
+  private final MutationContext      context;
+
+  UOIMutatorMethodVisitor(final MethodMutatorFactory factory,
+                                final MutationContext context, final MethodVisitor delegateMethodVisitor) {
+    super(Opcodes.ASM6, delegateMethodVisitor);
+    this.factory = factory;
+    this.context = context;
   }
 
-  private static final Map<Integer, ZeroOperandMutation> MUTATIONS = new HashMap<Integer, ZeroOperandMutation>();
 
-  static {
-    MUTATIONS.put(Opcodes.IADD, new InsnSubstitution(Opcodes.ISUB,
-            "Replaced integer addition with subtraction"));
-    MUTATIONS.put(Opcodes.ISUB, new InsnSubstitution(Opcodes.IADD,
-            "Replaced integer subtraction with addition"));
-    MUTATIONS.put(Opcodes.IMUL, new InsnSubstitution(Opcodes.IDIV,
-            "Replaced integer multiplication with division"));
-    MUTATIONS.put(Opcodes.IDIV, new InsnSubstitution(Opcodes.IMUL,
-            "Replaced integer division with multiplication"));
-    MUTATIONS.put(Opcodes.IOR, new InsnSubstitution(Opcodes.IAND,
-            "Replaced bitwise OR with AND"));
-    MUTATIONS.put(Opcodes.IAND, new InsnSubstitution(Opcodes.IOR,
-            "Replaced bitwise AND with OR"));
-    MUTATIONS.put(Opcodes.IREM, new InsnSubstitution(Opcodes.IMUL,
-            "Replaced integer modulus with multiplication"));
-    MUTATIONS.put(Opcodes.IXOR, new InsnSubstitution(Opcodes.IAND,
-            "Replaced XOR with AND"));
-    MUTATIONS.put(Opcodes.ISHL, new InsnSubstitution(Opcodes.ISHR,
-            "Replaced Shift Left with Shift Right"));
-    MUTATIONS.put(Opcodes.ISHR, new InsnSubstitution(Opcodes.ISHL,
-            "Replaced Shift Right with Shift Left"));
-    MUTATIONS.put(Opcodes.IUSHR, new InsnSubstitution(Opcodes.ISHL,
-            "Replaced Unsigned Shift Right with Shift Left"));
 
-    // longs
 
-    MUTATIONS.put(Opcodes.LADD, new InsnSubstitution(Opcodes.LSUB,
-            "Replaced long addition with subtraction"));
-    MUTATIONS.put(Opcodes.LSUB, new InsnSubstitution(Opcodes.LADD,
-            "Replaced long subtraction with addition"));
-    MUTATIONS.put(Opcodes.LMUL, new InsnSubstitution(Opcodes.LDIV,
-            "Replaced long multiplication with division"));
-    MUTATIONS.put(Opcodes.LDIV, new InsnSubstitution(Opcodes.LMUL,
-            "Replaced long division with multiplication"));
-    MUTATIONS.put(Opcodes.LOR, new InsnSubstitution(Opcodes.LAND,
-            "Replaced bitwise OR with AND"));
-    MUTATIONS.put(Opcodes.LAND, new InsnSubstitution(Opcodes.LOR,
-            "Replaced bitwise AND with OR"));
-    MUTATIONS.put(Opcodes.LREM, new InsnSubstitution(Opcodes.LMUL,
-            "Replaced long modulus with multiplication"));
-    MUTATIONS.put(Opcodes.LXOR, new InsnSubstitution(Opcodes.LAND,
-            "Replaced XOR with AND"));
-    MUTATIONS.put(Opcodes.LSHL, new InsnSubstitution(Opcodes.LSHR,
-            "Replaced Shift Left with Shift Right"));
-    MUTATIONS.put(Opcodes.LSHR, new InsnSubstitution(Opcodes.LSHL,
-            "Replaced Shift Right with Shift Left"));
-    MUTATIONS.put(Opcodes.LUSHR, new InsnSubstitution(Opcodes.LSHL,
-            "Replaced Unsigned Shift Right with Shift Left"));
 
-    // floats
-    MUTATIONS.put(Opcodes.FADD, new InsnSubstitution(Opcodes.FSUB,
-            "Replaced float addition with subtraction"));
-    MUTATIONS.put(Opcodes.FSUB, new InsnSubstitution(Opcodes.FADD,
-            "Replaced float subtraction with addition"));
-    MUTATIONS.put(Opcodes.FMUL, new InsnSubstitution(Opcodes.FDIV,
-            "Replaced float multiplication with division"));
-    MUTATIONS.put(Opcodes.FDIV, new InsnSubstitution(Opcodes.FMUL,
-            "Replaced float division with multiplication"));
-    MUTATIONS.put(Opcodes.FREM, new InsnSubstitution(Opcodes.FMUL,
-            "Replaced float modulus with multiplication"));
-
-    // doubles
-    MUTATIONS.put(Opcodes.DADD, new InsnSubstitution(Opcodes.DSUB,
-            "Replaced double addition with subtraction"));
-    MUTATIONS.put(Opcodes.DSUB, new InsnSubstitution(Opcodes.DADD,
-            "Replaced double subtraction with addition"));
-    MUTATIONS.put(Opcodes.DMUL, new InsnSubstitution(Opcodes.DDIV,
-            "Replaced double multiplication with division"));
-    MUTATIONS.put(Opcodes.DDIV, new InsnSubstitution(Opcodes.DMUL,
-            "Replaced double division with multiplication"));
-    MUTATIONS.put(Opcodes.DREM, new InsnSubstitution(Opcodes.DMUL,
-            "Replaced double modulus with multiplication"));
-
-  }
 
   @Override
-  protected Map<Integer, ZeroOperandMutation> getMutations() {
-    return MUTATIONS;
+  public void visitVarInsn(final int opcode, final int var) {
+
+    if (opcode == Opcodes.ILOAD) {
+      final MutationIdentifier newId = this.context.registerMutation(
+              this.factory, "Added increment to variable ");
+
+      if (this.context.shouldMutate(newId)) {
+        this.mv.visitIntInsn(Opcodes.ILOAD, var);
+        this.mv.visitIntInsn(Opcodes.BIPUSH, Opcodes.ICONST_1);
+        this.mv.visitInsn(Opcodes.IADD);
+        this.mv.visitVarInsn(Opcodes.ISTORE, var);
+        super.visitVarInsn(opcode, var);
+
+      } else {
+        super.visitVarInsn(opcode, var);
+      }
+    } else if(opcode == Opcodes.DLOAD){
+      final MutationIdentifier newId = this.context.registerMutation(
+              this.factory, "Added increment to variable ");
+
+      if (this.context.shouldMutate(newId)) {
+        this.mv.visitVarInsn(Opcodes.DLOAD, var);
+        this.mv.visitLdcInsn(new Double("1.0"));
+        this.mv.visitInsn(Opcodes.DADD);
+        this.mv.visitVarInsn(Opcodes.DSTORE, var);
+        super.visitVarInsn(opcode, var);
+
+      } else {
+        super.visitVarInsn(opcode, var);
+      }
+
+    }
+
+
+
+    else {
+      super.visitVarInsn(opcode, var);
+    }
+
+
+  }
+
+
+
+
+
+
+  @Override
+  public void visitIincInsn(final int var, final int increment) {
+
+
+
+
+        final MutationIdentifier newIde = this.context.registerMutation(this.factory,
+                "UOI Mutator: Removed unary operator of local variable");
+
+        if (this.context.shouldMutate(newIde)) {
+
+          this.mv.visitIincInsn(var, 0);
+
+        } else {
+          this.mv.visitIincInsn(var, increment);
+        }
+
+
+
+
+
   }
 
 }
